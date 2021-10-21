31ab6ac12075d5b42eeb44b8caa3e82643c306da
Gustavo Martins <gm357191@gmail.com>
2021-10-21 00:35:25 -0300

    modificando dados de cada pagina

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page')
-}
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais disponíveis para adoção',
+            background: 'allanimals.jpg'
+        }
+    });
+};
 
 export const dogs = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os cachorros disponíveis para adoção',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os gatos disponíveis para adoção',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os peixes disponíveis para adoção',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
