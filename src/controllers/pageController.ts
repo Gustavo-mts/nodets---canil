cafc1e657c97da11acdfc7fe263ff03ced78599c
Gustavo Martins <gm357191@gmail.com>
2021-10-21 00:19:12 -0300

    separando views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send('home está funcionado')
+    res.render('pages/page')
 }
 
 export const dogs = (req: Request, res: Response) => {
