6cb2555c4bcbd451b1c94b0b4e0c690133c5994d
Gustavo Martins <gm357191@gmail.com>
2021-10-21 14:27:20 -0300

    pet list no controller

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,43 +1,56 @@
 import { Request, Response } from "express";
 
 import {createMenuObject} from '../helpers/createMenuObjects';
+import {Pet} from '../models/pets';
 
 export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();
+
     res.render('pages/page', {
         menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais disponíveis para adoção',
-            background: 'allanimals.jpg'
+            background: 'allanimals.jpg',
+            list
         }
     });
 };
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog'); 
+
     res.render('pages/page', {
         menu: createMenuObject('dog'),
         banner: {
             title: 'Todos os cachorros disponíveis para adoção',
-            background: 'banner_dog.jpg'
+            background: 'banner_dog.jpg', 
+            list
         }
     });
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
+
     res.render('pages/page', {
         menu: createMenuObject('cat'),
         banner: {
             title: 'Todos os gatos disponíveis para adoção',
-            background: 'banner_cat.jpg'
+            background: 'banner_cat.jpg',
+            list
         }
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
+
     res.render('pages/page', {
         menu: createMenuObject('fish'),
         banner: {
             title: 'Todos os peixes disponíveis para adoção',
-            background: 'banner_fish.jpg'
+            background: 'banner_fish.jpg',
+            list
         }
     });
 }
